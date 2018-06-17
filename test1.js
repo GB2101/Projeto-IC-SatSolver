9c5ec0891f912d54abf108d34465b4d91640736b
GB2101 <gbcs@cin.ufpe.br>
2018-06-16 22:31:41 -0300

    Finnaly

--- test1.js
+++ test1.js
@@ -1,19 +1,67 @@
-var array = [false, false, false, false]
+var array = [false, false, false, false, false, false, false, false]
+var x = Math.pow(2, array.length) + 1
+while(x > 0){
+    console.log(array)
+    array = newArray(array)
+    x--
+}
 
-function newArray(array){
+function newArray(array) {
+    if (!array[0]) {
+        array = change(array, 1).array
+    } else {
+        array = reset(array)
+    }
+    return array
+}
+
+function change(array, pos) {
     var aux = false
-    if (!array[x]){
-        for(i = 1; i < array.length; i++){
-            if (array[i]){
+    var k = 0
+    for (var i = pos; i < array.length; i++) {
+        if (array[i]) {
+            array[i] = false
+            k = i - 1
+            array[k] = true
+            aux = true
+            i = array.length
+        }
+    }
+    if (!aux) {
+        k = array.length - 1
+        array[k] = true
+    }
+    var res = { array, k }
+    return res
+}
+
+function reset(array) {
+    var aux
+    var k
+    var set
+    var aux2 = false
+    for (var i = 0; i < array.length; i++) {
+        if (!array[i]) {
+            aux = change(array, i)
+            array = aux.array
+            k = aux.k
+            set = i
+            aux2 = true
+            i = array.length
+        }
+    }
+    if (aux2) {
+        for (var i = k - 1; i >= 0; i--) {
+            if (set > 0) {
+                array[i] = true
+                set = set - 1
+            } else {
                 array[i] = false
-                array[i-1] = true
-                aux = true
-                i = array.length
             }
         }
-        if(!aux){
-            var k = array.length - 1
-            array[k] = true
+    }else{
+        for(var i = 0; i < array.length; i++){
+            array[i] = false
         }
     }
     return array
