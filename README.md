7c8c0475a46b9beb1150ab9a4924f51235f611d1
GB2101 <gbcs@cin.ufpe.br>
2018-06-18 21:07:29 -0300

    18/06/2018 21:08 - Arquivos deletados, main atualizado

--- README.md
+++
@@ -1,4 +0,0 @@
-# Projeto-IC-SatSolver
-O projeto foi feito para ser rodado em node.js
-O arquivo principal é o 'main.js
-É atraves do arquivo 'entrada' que seleciona o arquivo para ser lido
--- main.js
+++ main.js
@@ -1,19 +1,8 @@
 var sat = require('./satSolver.js')
 
-var name = fileName()
+var name = 'simple0'
 
 var result = sat.solve(name)
 
 console.log(result.isSat)
 console.log(result.satisfyingAssignment)
-
-function fileName(){
-    var fs = require('fs')
-    var name = fs.readFileSync('entrada', 'utf8')
-    name = name.split('\r\n')
-    name = name[3].split(' ')
-    var x = name.length - 1
-    name = name[x]
-    name = 'files/'+ name +'.cnf'
-    return name
-}
